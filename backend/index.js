106172ab8532f4848fbc0782d0fae072d84c9dd5
Coop <coop.coding@gmail.com>
2020-02-10 15:52:22 +1100

    update

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,28 @@
+const http = require('http')
 const path = require('path')
 
 const express = require('express')
 const { Liquid } = require('liquidjs')
 const engine = new Liquid()
 
-
+const instant = require('instant')
 const app = express()
+const server = http.createServer(app)
+
+const publicDir = path.resolve(__dirname, '..', 'frontend')
 
 app.engine('liquid', engine.express())
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'liquid')
-app.use(express.static(path.resolve(__dirname, '..', 'frontend')))
+app.use(instant(publicDir))
 
 const port = 8887
-var http = require('http')
 app.get('/', (req, res) => res.render('index'))
 var internalIp = require('internal-ip')
 // app.listen(port, '192.168.1.2', () => console.log(`Example app listening on port ${port}!`))
 
 
 
-var server = http.createServer(app)
 
 var ipadd = internalIp.v4.sync()
 server.listen(port, ipadd)
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,2 +1,2 @@
 console.log('asd')
-console.log('reesssss11sssrt')
+console.log('22')
