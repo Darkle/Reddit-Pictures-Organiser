9eded0960d6b971e61c0e1c4267f575c28248095
Coop <coop.coding@gmail.com>
2020-02-21 19:18:36 +1100

    Switched to localforage, statezero and yo-yo

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,25 +1,84 @@
-import { observable, html } from './web_modules/sinuous.js'
+import yo from './web_modules/yo-yo.js'
+import localforage from './web_modules/localforage.js'
+import { action, subscribe, setState } from './web_modules/statezero.js'
 
-const oneSecondInMS = 1000
-const seconds = observable(0)
-const Timer = props => {
-  // Create an obervable with number `0`.
+localforage.config({
+  name: 'Reddit Pictures Organiser'
+})
+localforage.setItem('key', 'valueww').catch(err => console.error(err))
 
-  function tick() {
-    // Get the current value and increment the value with 1.
-    seconds(seconds() + 1)
+// folders state structure
+/*
+folders : {
+  'folder 1':{
+      'https://reddit post url' : {
+
+      },
+      'https://reddit post url 2' : {
+
+      },
   }
-  setInterval(tick, oneSecondInMS)
-
-  // Creates the view of this component.
-  return html`
-    <div>${props.unit}: ${seconds}</div>
-  `
 }
+*/
+// and to get all folders, just us Object.keys(folders)
+// to get folder images would just be folders['folder 1'].images
 
-document.querySelector('#app').append(
-  // Use the component and pass some props.
-  html`
-    <${Timer} unit="Seconds" />
-  `
-)
+setState('folders', foldersArrayWeGotFromLocalForage)
+
+const addFolder = action(({ commit, state }, newFolder) => {
+  state.folders[newFolder] = {}
+  commit(state)
+})
+
+const removeFolder = action(({ commit, state }, folderToRemove) => {
+  delete state.folders[folderToRemove]
+  commit(state)
+})
+
+const addImageToFolder = action(({ commit, state }, folder, image) => {
+  const imageRedditPostUrl = image.thing
+  state.folders[folder][imageRedditPostUrl] = image
+  commit(state)
+})
+
+const removeImageFromFolder = action(({ commit, state }, folder, imageToRemove) => {
+  const imageRedditPostUrl = imageToRemove.thing
+  delete state.folders[folder][imageRedditPostUrl]
+  commit(state)
+})
+
+// subreddits structure
+// [
+//   'sub1',
+//   'sub2'
+// ]
+
+setState('subreddits', subsArrayWeGotFromLocalForage)
+
+const addSubreddit = action(({ commit, state }, newSub) => {
+  state.subreddits.push(newSub)
+  commit(state)
+})
+
+const removeSubreddit = action(({ commit, state }, subToRemove) => {
+  //state.subreddits.push(newSub) omit sub
+  commit(state)
+})
+
+// favouriteSubreddits structure
+// [
+//   'sub1',
+//   'sub2'
+// ]
+
+setState('favouriteSubreddits', faveSubsArrayWeGotFromLocalForage)
+
+const addFavSubreddit = action(({ commit, state }, newFavSub) => {
+  state.subreddits.push(newSub)
+  commit(state)
+})
+
+const removeFavSubreddit = action(({ commit, state }, subToRemove) => {
+  //state.subreddits.push(newSub) omit sub
+  commit(state)
+})
