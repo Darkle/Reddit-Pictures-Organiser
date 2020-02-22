a460ff635b864f42ea2435f6799bfd3cd6e35c0f
Coop <coop.coding@gmail.com>
2020-02-22 13:48:39 +1100

    Update

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,97 +1,25 @@
-import yo from './web_modules/yo-yo.js'
+import html from './web_modules/yo-yo.js'
 import localforage from './web_modules/localforage.js'
-import createNanoEvents from './web_modules/nanoevents.js'
+
+import {emitter} from './state.js'
 
 localforage.config({
   name: 'Reddit Pictures Organiser'
 })
 localforage.setItem('key', 'valueww').catch(err => console.error(err))
 
-const emitter = createNanoEvents()
-
-// folders state structure
-/*
-folders : {
-  'folder 1':{
-      'https://reddit post url' : {
-
-      },
-      'https://reddit post url 2' : {
-
-      },
-  }
+function list (items) {
+  return html`<ul>
+    ${items.map(function (item) {
+      return html`<button onmouseup=${ () => emitter.emit('add-subreddit', item) }>${item}</button>`
+    })}
+  </ul>`
 }
-*/
-// and to get all folders, just us Object.keys(folders)
-// to get folder images would just be folders['folder 1'].images
-var el = list([
+const el = list([
   'grizzly',
   'polar',
   'brown'
 ])
 
-function list (items) {
-  return yo`<ul>
-    ${items.map(function (item) {
-      return yo`<li>${item}</li>`
-    })}
-  </ul>`
-}
-
-document.body.appendChild(el)
-
-const state = {
-  folders : {
-    'folder 1':{
-      // foldersArrayWeGotFromLocalForage
-      'https://reddit post url' : {
-
-      },
-      'https://reddit post url 2' : {
-
-      },
-    }
-  },
-  subreddits: [ //subsArrayWeGotFromLocalForage
-    'foo',
-    'bar',
-  ],
-  favouriteSubreddits: [ //faveSubsArrayWeGotFromLocalForage
-    'foo',
-    'bar',
-  ]
-}
-
-emitter.on('add-folder', newFolder => {
-  state.folders[newFolder] = {}
-})
-
-emitter.on('remove-folder', folderToRemove => {
- delete state.folders[folderToRemove]
-})
-
-emitter.on('add-image-to-folder', (folder, image) => {
-  const imageRedditPostUrl = image.thing
-  state.folders[folder][imageRedditPostUrl] = image
-})
-
-emitter.on('remove-image-from-folder', (folder, imageToRemove) => {
-  const imageRedditPostUrl = imageToRemove.thing
-  delete state.folders[folder][imageRedditPostUrl]
-})
-
-emitter.on('add-subreddit', newSub => {
-  state.subreddits.push(newSub)
-})
-
-emitter.on('remove-subreddit', subToRemove => {
-  state.subreddits = state.subreddits.filter(sub => sub !== subToRemove)
-})
-
-emitter.on('add-favourite-subreddit', newSub => {
-  state.favouriteSubreddits.push(newSub)
-})
-
-emitter.on('remove-favourite-subreddit', subToRemove => {
-  state.favouriteSubreddits = state.favouriteSubreddits.filter(sub => sub !== subToRemove)
-})
+document.querySelector('#app').appendChild(el)
+console.log('foo')
