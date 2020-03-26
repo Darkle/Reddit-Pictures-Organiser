9731c4db70aee3f0c7be4f8fb9f988de01046271
Coop <coop.coding@gmail.com>
2020-03-26 17:28:52 +1100

    Comments

--- src/js/views/imageEditorPage/Nav.js
+++ src/js/views/imageEditorPage/Nav.js
@@ -39,7 +39,7 @@ function Nav(subreddit, timefilter, imageId){ // eslint-disable-line max-lines-p
 function handleBackNavigation(subreddit, timefilter, imageId){
   cancelEditsOnNavAway()
   router.navigate(`/sub/${subreddit}/${timefilter}/imageviewer/${imageId}`)
-  // ⬇ superfine patch is severely broken ⬇
+  // ⬇ superfine patch is severely broken, so... ⬇
   requestAnimationFrame(() => $('#app').firstChild.nextSibling.remove())
 }
 
