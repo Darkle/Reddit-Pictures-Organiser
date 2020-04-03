fd1daa58f8c2dd2ba7a04fbf47d5f6c96f98a9b3
Coop <coop.coding@gmail.com>
2020-04-03 21:31:07 +1100

    Updated service worker.

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,6 +1,13 @@
-self.addEventListener('install', () => {
+ self.addEventListener('install', (event) => {
   console.log('service worker installed')
- })
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/index.html',
+      ])
+    })
+  )
+})
  self.addEventListener('fetch', function(event) {
   event.respondWith(caches.match(event.request))
 })
