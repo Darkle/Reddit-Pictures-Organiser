72de7f6da27e0977dd6968864b7501c942072a28
Coop <coop.coding@gmail.com>
2020-04-03 21:55:34 +1100

    Build

--- dist/js/sw.js
+++ dist/js/sw.js
@@ -1,3 +1,16 @@
-self.addEventListener('install',(event)=>{console.log('service worker installed')
-event.waitUntil(caches.open('v1').then((cache)=>{return cache.addAll(['/','./','/index.html','./index.html',])}))})
-self.addEventListener('fetch',function(event){event.respondWith(caches.match(event.request))})
+ self.addEventListener('install', (event) => {
+  console.log('service worker installed')
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        './',
+        '/index.html',
+        './index.html',
+      ])
+    })
+  )
+})
+ self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request))
+})
