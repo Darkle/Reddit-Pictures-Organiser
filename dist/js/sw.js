b1013eeef9d2029a6e14ca1ed74204142e7a15dc
Coop <coop.coding@gmail.com>
2020-04-03 21:50:55 +1100

    Updated build.

--- dist/js/sw.js
+++ dist/js/sw.js
@@ -1,1 +1,3 @@
-console.log('sw here')
+self.addEventListener('install',(event)=>{console.log('service worker installed')
+event.waitUntil(caches.open('v1').then((cache)=>{return cache.addAll(['/',])}))})
+self.addEventListener('fetch',function(event){event.respondWith(caches.match(event.request))})
--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,5 @@ if(!window.location.hash.includes('#')) window.location.hash = '!/' // eslint-di
 initDb().then(initRouter).catch(logger.error)
 // service worker path is relative to the html file
 if('serviceWorker' in navigator){
-  navigator.serviceWorker.register('/js/service-worker.js').catch(logger.error)
+  navigator.serviceWorker.register('/js/sw.js').catch(logger.error)
 }
