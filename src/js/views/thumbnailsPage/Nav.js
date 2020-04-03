c57cbe0ba5a2007df6ac25b17f0a49d4febe9a04
Coop <coop.coding@gmail.com>
2020-04-03 15:27:14 +1100

    Added navigation and folder delete to folder thumbnail view.

--- src/js/views/thumbnailsPage/Nav.js
+++ src/js/views/thumbnailsPage/Nav.js
@@ -19,6 +19,17 @@ function Nav(state){ // eslint-disable-line max-lines-per-function, complexity
             <path d="M15 18l-6-6 6-6"></path>
           </svg> 
        </div>
+       <div class="folderName">${state.folderpage}</div>
+       <div class="removeImageFromFolder" 
+          @mouseup=${() => deleteFolder(state)}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" 
+            stroke="#fff4e8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+          <polyline points="3 6 5 6 21 6"></polyline>
+          <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+          <line x1="10" y1="11" x2="10" y2="17"></line>
+          <line x1="14" y1="11" x2="14" y2="17"></line>
+        </svg>
+      </div>
       </nav>  
     `
   }
@@ -48,6 +59,11 @@ function FavStar(subreddit){
         <path fill-rule="evenodd" d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"></path>
       </svg>
   `
+}
+
+function deleteFolder(state){
+  store.removeFolder(state.folderpage)
+  router.navigate(`/folders/`)
 }
 
 function toggleSubAsFavourite(state){
