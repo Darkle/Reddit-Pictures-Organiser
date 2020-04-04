6fb017eac14543c0621f685b05549ef5be55c265
Coop <coop.coding@gmail.com>
2020-04-04 15:48:33 +1100

    Reorganised build scripts.

--- src/sw.js
+++ src/sw.js
@@ -1,18 +1,24 @@
 /*****
   This file needs to be in the site root to capture requests from root https://developers.google.com/web/fundamentals/primers/service-workers#register_a_service_worker
 *****/
-self.addEventListener('install', (event) => {
-  console.log('service worker installed')
+// self.addEventListener('install', (event) => {
+//   console.log('service worker installed')
+//   // @ts-ignore
+//   event.waitUntil(
+//     caches.open('v1').then((cache) => {
+//       return cache.addAll([
+//         '/',
+//       ])
+//     })
+//   )
+// })
+self.addEventListener('fetch', function(event) {
   // @ts-ignore
-  event.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll([
-        '/',
-      ])
+  event.respondWith(
+    // @ts-ignore
+    fetch(event.request).catch(function() {
+      // @ts-ignore
+      return caches.match(event.request)
     })
   )
-})
- self.addEventListener('fetch', function(event) {
-   // @ts-ignore
-  event.respondWith(caches.match(event.request))
 })
