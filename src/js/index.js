4eb1082d43b500bfbfff1f43c58345ff04fa7ce5
Coop <coop.coding@gmail.com>
2020-04-04 12:43:02 +1100

    Changed cursor icon for desktop for nav bar and folders and subreddits and thumbnails. Added hover color for nav and folders and subs and thumbnails.

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,6 @@ if(!window.location.hash.includes('#')) window.location.hash = '!/' // eslint-di
 
 initDb().then(initRouter).catch(logger.error)
 // service worker path is relative to the html file
-if('serviceWorker' in navigator){
-  navigator.serviceWorker.register('/js/sw.js').catch(logger.error)
-}
+// if('serviceWorker' in navigator){
+//   navigator.serviceWorker.register('/js/sw.js').catch(logger.error)
+// }
