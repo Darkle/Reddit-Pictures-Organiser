353927c33a451dcb109f4b1dbb13b6b75aa071e5
Coop <coop.coding@gmail.com>
2020-04-07 09:40:27 +1000

    Updated service worker.

--- dist/sw.js
+++ dist/sw.js
@@ -1,1 +1,4 @@
-self.addEventListener('fetch',function(event){})
+self.addEventListener('install',function(event){event.waitUntil(caches.open('v1').then(function(cache){console.log('Opened cache')
+return cache.addAll(['/',])}))})
+self.addEventListener('fetch',function(event){event.respondWith(caches.match(event.request).then(function(response){if(response){return response}
+return fetch(event.request)}))})
--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,31 @@
 /*****
   This file needs to be in the site root to capture requests from root https://developers.google.com/web/fundamentals/primers/service-workers#register_a_service_worker
 *****/
-self.addEventListener('fetch',function(event){
-
+self.addEventListener('install', function(event) {
+  // @ts-ignore
+  event.waitUntil(
+    caches.open('v1')
+      .then(function(cache) {
+        console.log('Opened cache')
+        return cache.addAll([
+          '/',
+        ])
+      })
+  )
+})
+self.addEventListener('fetch', function(event) {
+  // @ts-ignore
+  event.respondWith(
+    // @ts-ignore
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response
+        }
+        // @ts-ignore
+        return fetch(event.request)
+      }
+    )
+  )
 })
