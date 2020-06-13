5562cd161c8670557d7fcfa3477e513edccc8e10
Coop <coop.coding@gmail.com>
2020-06-14 09:59:57 +1000

    Update

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,7 +1,6 @@
 import{init as initDb}from './store/db.js'
 import{logger}from './logger.js'
 import{initRouter}from './router.js'
-import{store}from './store/store.js'
 if(!window.location.hash.includes('#'))window.location.hash='!/'
-initDb().then(initRouter).then(()=>{console.log(JSON.stringify(store.subreddits))}).catch(logger.error)
+initDb().then(initRouter).catch(logger.error)
 if('serviceWorker'in navigator){navigator.serviceWorker.register('sw.js').catch(logger.error)}
